2dd04075d653bda93e134f9a0b9d04739f0b44a5
bin2chen66 <bin2chen66@gmail.com>
2022-05-21 09:02:00 +0800

    07 08 file error bug

--- contracts/08VaultRun.sol
+++ contracts/08VaultRun.sol
@@ -1,9 +1,3 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
-
-contract ForceRun {
-    function destory(address _levelAddress) external payable {
-        //selfdestruct指定的地址，就算没有receive/fallback也是无法拒接把balance转给它
-        selfdestruct(payable(_levelAddress));
-    }
-}
+//不需要solidity,js调用关卡合约即可
