b4fa100a7220fcc16ef722725e29398982af3fd8
bin2chen66 <bin2chen66@gmail.com>
2022-06-01 10:49:10 +0800

    update 19 run.sol

--- contracts/19AlienCodexRun.sol
+++ contracts/19AlienCodexRun.sol
@@ -17,7 +17,20 @@ contract AlienCodexRun {
     level.make_contact();
     //先回退,0.5版本会溢出，数组长度变很长
     level.retract();
-    //数组的起始slot，(solt:codex[x]) = keccak256(1)+x
+    /**
+        Slot                  Data
+        ------------------------------
+        0                     owner, contact
+        1                     codex.length      #p = 1
+        .
+        .
+        keccak256(p)          codex[0]     
+        keccak256(p) + 1      codex[1]
+        .
+        .
+        keccak256(p) + index  codex[index]
+.
+     */   
     bytes32 arrSlot = keccak256(abi.encodePacked(uint256(1)));    
     uint256 gap = (type(uint256).max - uint256(arrSlot)) + 1;
     //传入GAP会导致arrSlot+gag溢出后=0，即设置slot:0,这个位置用于存owner和bool contact
